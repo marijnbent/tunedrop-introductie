eef0b09c58f17c3e2212086d6d48872c8975b4d1
JeremyPrins <jeremyprins@outlook.com>
2015-08-11 14:54:43 +0200

    Changed index.html and some css

--- assets/js/googlemaps.js
+++ assets/js/googlemaps.js
@@ -1,24 +1,12 @@
 var map;
 
-$(init);
-
-function init() {
-    mapLoad();
-    mapHeight();
-    //Also sets mapHeight for when all elements are loaded (without this it can result in the map not filling the screen properly due to images not having loaded yet)
-    //It also resizes when you resize the window.
-    $(window).on('load', mapHeight);
-    $(window).on('resize', mapHeight);
-}
-
-function mapLoad() {
-    // This is the minimum zoom level that we'll allow
     var minZoomLevel = 13;
     var maxZoomLevel = 16;
 
+function initialize() {
     var mapOptions = {
         center: {lat: 51.911722, lng: 4.463599},
-        zoom: 14,
+        zoom: 12,
         minZoom: minZoomLevel,
         maxZoom: maxZoomLevel,
         mapTypeId: "customMapStyle",
@@ -30,10 +18,4 @@ function mapLoad() {
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     map.mapTypes.set('customMapStyle', styledMap);
 }
-
-function mapHeight() {
-    //Sets height for map: Total size of window minus the form in the top and 2 pixels due to borders set in css
-    var mapHeight = $(window).height();
-    $('#map-canvas').height(mapHeight);
-
-}
+google.maps.event.addDomListener(window, 'load', initialize);
