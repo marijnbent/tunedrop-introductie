8c07c63a885d3cabc0868c5787f94c8486f5dc44
Flurokazoo <jasper_thecrown@hotmail.com>
2015-08-11 14:18:55 +0200

    Added grid-generation logic with X and Y coordinates
    Dat dus

---
+++ inserttodatabase/createGrid.js
@@ -0,0 +1,55 @@
+var latStart = [];
+var lngStart = [];
+var latEnd = [];
+var lngEnd = [];
+var x = [];
+var y = [];
+
+$(init);
+
+function init() {
+
+    var dim = 0.0025;
+    var dim2 = 0.004;
+    for (var i = 0; i < 25; i++) {
+        for (var j = 0; j < 25; j++) {
+            latStart.push(51.880173 + (i * dim));
+            lngStart.push(4.412449 + (j * dim2));
+            latEnd.push(51.880173 + dim + (i * dim));
+            lngEnd.push(4.412449 + dim2 + (j * dim2));
+            x.push(i + 1);
+            y.push(j + 1);
+        }
+    }
+    console.log(y);
+    //ajaxWriteToDatabase();
+    fillTable();
+}
+
+function fillTable() {
+
+    for (var i = 0; i < latStart.length; i++) {
+        $("#grid-table")
+            .append($('<tr>')
+                .append($('<td>')
+                    .text(latStart[i])
+                )
+                .append($('<td>')
+                    .text(lngStart[i])
+                )
+                .append($('<td>')
+                    .text(latEnd[i])
+                )
+                .append($('<td>')
+                    .text(lngEnd[i])
+                )
+                .append($('<td>')
+                    .text(x[i])
+                )
+                .append($('<td>')
+                    .text(y[i])
+                )
+            )
+
+    }
+}
---
+++ inserttodatabase/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+
+<table style="width:100%" id="grid-table">
+    <tr>
+        <th>Latstart</th>
+        <th>Lngstart</th>
+        <th>Latend</th>
+        <th>Lngend</th>
+        <th>X ></th>
+        <th>Y V</th>
+    </tr>
+
+</table>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="createGrid.js"></script>
+</body>
+</html>
+
