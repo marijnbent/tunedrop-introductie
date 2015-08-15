f8e2d294ba32e223389269977c40f6e9a5449514
Flurokazoo <jasper_thecrown@hotmail.com>
2015-08-15 15:18:02 +0200

    Added square-owner logic

--- assets/php/ajaxCalls.php
+++ assets/php/ajaxCalls.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once("config.php");
+require_once("db.php");
+
 
 if (isset($_GET['config'])) {
-
 	$c = $_GET['config'];
 	switch ($c) {
 		//Gets the general grid as a playing field
@@ -11,15 +11,13 @@ if (isset($_GET['config'])) {
 			$count = 0;
 			$jsonArray = [];
 			$query = "SELECT g.id, g.latStart, g.latEnd, g.lngStart, g.lngEnd, g.X, g.Y FROM grids g";
-
-			$queryResult = mysqli_query($connect, $query);
+			$queryResult = queryToDatabase($connect, $query);
 			foreach ($queryResult as $row) {
 				$jsonArray[$count]['gridId'] = $row['id'];
 				$jsonArray[$count]['latStart'] = $row['latStart'];
 				$jsonArray[$count]['lngStart'] = $row['lngStart'];
 				$jsonArray[$count]['latEnd'] = $row['latEnd'];
 				$jsonArray[$count]['lngEnd'] = $row['lngEnd'];
-
 				$jsonArray[$count]['x'] = $row['X'];
 				$jsonArray[$count]['y'] = $row['Y'];
 				$count++;
@@ -30,6 +28,14 @@ if (isset($_GET['config'])) {
 
 			break;
 		case 1:
+            //Gets the team id for the grid you're currently in
+            $currentGrid = $_GET['currentgrid'];
+            $query = "SELECT teamId FROM grids WHERE id = " . $currentGrid . ";";
+            $queryResult = queryToDatabase($connect, $query);
+            $queryResult = queryToArray($queryResult);
+            header('Content-Type: application/json');
+            echo json_encode($queryResult);
+            exit;
 
 			break;
 		case 2:
