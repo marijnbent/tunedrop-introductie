59a5b4fdc9ae28c5b39d7cc34b11c7e3fbb603ce
marijnbent <marijnbent@gmail.com>
2015-08-18 16:56:37 +0200

    Upload images

--- uploadtest.php
+++ uploadtest.php
@@ -1,16 +1,50 @@
+<?php
+
+require_once("assets/php/config.php");
+require_once("assets/php/db.php");
+
+if(isset($_FILES['image'])){
+	$errors= array();
+	$file_name = $_FILES['image']['name'];
+	$file_size =$_FILES['image']['size'];
+	$file_tmp =$_FILES['image']['tmp_name'];
+	$file_type=$_FILES['image']['type'];
+	$file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+	$extensions = array("jpeg","jpg","png");
+
+	if(in_array($file_ext,$extensions )=== false){
+		$errors[]="extension not allowed, please choose a JPEG or PNG file.";
+    }
+
+	if($file_size > 5097152){
+		$errors[]='File size must be around 5 MB';
+	}
+	if(empty($errors)==true){
+		define ('SITE_ROOT', realpath(dirname(__FILE__)));
+		move_uploaded_file($file_tmp,SITE_ROOT . "/assets/img/uploaded/" . $file_name);
+		$path = "/github/tunedrop-introductie/assets/img/uploaded/" . $file_name;
+		echo "Success: <img src='" . $path . "'/>";
+	}else{
+		print_r($errors);
+	}
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
-    <meta charset="UTF-8">
-    <meta name="description" content=""/>
-    <link rel="stylesheet" href=""/>
-    <title>Upload Test</title>
+	<meta charset="UTF-8">
+	<meta name="description" content=""/>
+	<link rel="stylesheet" href=""/>
+	<title>Upload Test</title>
 </head>
 <body>
 
-<form name="form" method="POST" action="assets/php/getdata.php" enctype="multipart/form-data">
-    <input type="file" name="myimage"  id="myimage">
-    <input type="submit" name="submit_image" value="Upload">
+<form action="" method="POST" enctype="multipart/form-data">
+	<input type="file" name="image" />
+	<input type="submit"/>
 </form>
 
 </body>
